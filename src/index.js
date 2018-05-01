3480377be1ca1e41d23ed04037033bad99060838
DEVSDone <patil.dhiraj101@gmail.com>
2018-05-01 23:49:48 +0530

    multiple url

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import Flexboxgrid from "flexboxgrid";
 import SitePane from "./components/sitepane.js";
-
-
+import "./styles.css";
 
 const App = () => (
   <MuiThemeProvider>
-    <SitePane/>
+    <div className="row">
+      <div className="col-xs"><SitePane /></div>
+      <div className="col-xs"><SitePane /></div>
+    </div>
+    <div className="row">
+      <div className="col-xs"><SitePane /></div>
+      <div className="col-xs"><SitePane /></div>
+    </div>
   </MuiThemeProvider>
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
