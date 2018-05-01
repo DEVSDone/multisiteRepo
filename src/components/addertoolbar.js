8fe46673dd62403f92dc59648567060830d3a064
DEVSDone <patil.dhiraj101@gmail.com>
2018-05-02 00:17:17 +0530

    added button

---
+++ src/components/addertoolbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import FlatButton from 'material-ui/FlatButton';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+
+const style = {
+  marginRight: 20,
+};
+class AdderToolbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="row end-xs">
+      <div className="col-xs">
+        <FloatingActionButton secondary={true} style={style}>
+          <ContentAdd />
+        </FloatingActionButton>
+      </div>
+    </div>
+  }
+}
+
+export default AdderToolbar;
