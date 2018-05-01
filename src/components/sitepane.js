fce2c11cfc1e4d9e5ed10936d9c98abfa9451f96
DEVSDone <patil.dhiraj101@gmail.com>
2018-05-01 23:32:53 +0530

    loaded URl

--- src/components/sitepane.js
+++ src/components/sitepane.js
@@ -6,14 +6,40 @@ const style = {
 };
 
 class SitePane extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {url:"",selectedURL:""};
+    this.handleChange = this.handleChange.bind(this);
+    this.setSelectedURL = this.setSelectedURL.bind(this);
+  }
+  handleChange = (event) => {
+    this.setState({
+      url: event.target.value,
+    });
+  };
+  setSelectedURL(){
+    let { url} = this.state;
+    if (url){
+      this.setState({ selectedURL:url})
+    }else{
+      alert("Input URL first...")
+    }
+  }
   render() {
-    return <div>
+    return <div className="parent">
       <TextField
+        value={this.state.url}
+        onChange={this.handleChange}
         hintText="Enter URL here"
       />
       <RaisedButton label="Go"
         primary={true}
+        onClick = {this.setSelectedURL}
         style={style}
+      />
+      <iframe src={this.state.selectedURL}
+       height="100%"
+      width="100%"
       />
     </div>
   }
