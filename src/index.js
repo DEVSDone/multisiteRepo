2d7345505e2d3d7cb27f033dc64fe9ff3693b63a
DEVSDone <patil.dhiraj101@gmail.com>
2018-05-02 00:30:11 +0530

    add button

--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@ import ReactDOM from "react-dom";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import Flexboxgrid from "flexboxgrid";
 import SitePane from "./components/sitepane.js";
+import AdderToolbar from "./components/addertoolbar.js";
 import "./styles.css";
 
-const App = () => (
-  <MuiThemeProvider>
-    <div className="row center-xs parent">
-      <div className="col-xs parent border"><SitePane /></div>
-      <div className="col-xs parent border"><SitePane /></div>
-    </div>
-  </MuiThemeProvider>
-);
-
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { currentGrid: [] };
+  }
+  addrGridItem() {
+    let { currentGrid } = this.state;
+    currentGrid.push(<div className="col-xs parent border"><SitePane /></div>);
+    this.setState({ currentGrid });
+  }
+  render() {
+    return (
+      <MuiThemeProvider>
+        <AdderToolbar />
+        <div className="row center-xs parent">
+          {this.state.currentGrid}
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+}
 ReactDOM.render(<App />, document.getElementById("root"));
